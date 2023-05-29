eb8d2b7e0eb32a0dee5a6a840ae84670ebb80618
Yuliya Feland <yuliyakf@gmail.com>
2023-05-28 23:23:27 -0400

    some updates

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 // https://pro.openweathermap.org/data/2.5/forecast/climate?id="+value+"&appid=607d51094b8d7505384d31dc74a84976
 
-let cityInput = document.getElementById("cityInput").value;
+let cityInput = document.getElementById("cityInput");
 let searchBtn = document.getElementById("searchBtn");
 let cityName = document.querySelector(".cityName");
-
+let eventContainer=document.getElementById("event-container")
 searchBtn.addEventListener("click", searchFunc);
 
 function searchFunc(){
     if (searchBtn){
         fetchDataEvents(cityInput.value)
-        fetchDataWeather(cityInput.value)
-        setStorage()
+        //fetchDataWeather(cityInput.value)
+        //setStorage()
     } cityInput.value=""
 }
 
-function fetchDataEvents(cityInput){
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?size=31&apikey=GC2GWOqVAojsGdOJA1N1FM1RbT4Hzc94")
-    .then((res)=>res.json())
-    .then((discovery)=>{
-        console.log(discovery)
+function fetchDataEvents(value) {
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=['" + value + "']&size=30&sort=date,desc&apikey=GC2GWOqVAojsGdOJA1N1FM1RbT4Hzc94", {
+        method: 'GET',
     })
+        .then((res) => res.json())
+        .then((data) => {
+            cityName.innerHTML = data._embedded.events[0]._embedded.venues[0].city.name
+            let events= data._embedded.events;
+            events.forEach(event => {
+                console.log(event);
+                let cardObject = document.createElement('div');
+                cardObject.className = 'card';
+                cardObject.innerHTML = event.name;
+                eventContainer.appendChild(cardObject);
+            });
+        })
 }
 
-function fetchDataWeather(value){
+// function fetchDataWeather(value){
 
-}
+// }
 
-function setStorage(){
+// function setStorage(){
 
-}
+// }
 
 // fetch("https://app.ticketmaster.com/discovery/v2/events.json?size=31&apikey=GC2GWOqVAojsGdOJA1N1FM1RbT4Hzc94")
 //     .then((res)=>res.json())
@@ -37,3 +47,22 @@ function setStorage(){
     // fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=charlotte&apikey=GC2GWOqVAojsGdOJA1N1FM1RbT4Hzc94")
     // .then((res)=>res.json())
     // .then(console.log)
+
+
+    // function fetchDataEvents(value) {
+    //     fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=['" + value + "']&size=30&sort=date,desc&apikey=GC2GWOqVAojsGdOJA1N1FM1RbT4Hzc94", {
+    //         method: 'GET',
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             cityName.innerHTML = data._embedded.events[0]._embedded.venues[0].city.name
+    //             let events= data._embedded.events;
+    //             events.forEach(event => {
+    //                 console.log(event);
+    //                 let cardObject = document.createElement('div');
+    //                 cardObject.className = 'card';
+    //                 cardObject.innerHTML = event.name;
+    //                 eventContainer.appendChild(cardObject);
+    //             });
+    //         })
+    // }
